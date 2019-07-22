cedd4cd8fb9b55b81029101ca6c8f80329eff07b
Martino Wagey <martinowagey@gmail.com>
2019-07-22 16:55:14 +0700

    add tugas 15 JS

---
+++ tugas15.js
@@ -0,0 +1,10 @@
+function pythagoras(a,b) {
+  console.log("Nilai AB : ",a);
+  console.log("Nilai BC : ",b);
+
+  var c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+  console.log("Panjang Sisi AC Pada segitiga Siku Siku tersebut adalah : ",c," cm");
+
+}
+
+pythagoras(8,6);
